b371c5d9a6471dec8a476cb6b79cb836a92dd9aa
brunomarquete <brunomarquete@gmail.com>
2018-05-25 16:05:30 -0300

    Ajustes

--- projeto/front-end/src/providers/event-logger/event-logger.ts
+++ projeto/front-end/src/providers/event-logger/event-logger.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { FirebaseAnalytics } from '@ionic-native/firebase-analytics';
+import { Platform } from 'ionic-angular';
 
 @Injectable()
 export class EventLoggerProvider {
 
-  constructor(public firebaseAnalytics: FirebaseAnalytics) {
+  constructor(public firebaseAnalytics: FirebaseAnalytics, private platform: Platform) {
     console.log('Hello EventLoggerProvider Provider');
   }
  
   log(name:string,value:any){
-    this.firebaseAnalytics.logEvent(name, { pram:value })
+    if (this.platform.is('cordova')) {
+      this.firebaseAnalytics.logEvent(name, { pram:value })
+      .then((res: any) => console.log(res))
+      .catch((error: any) => console.error(error));
+    }
   }
 
 }
